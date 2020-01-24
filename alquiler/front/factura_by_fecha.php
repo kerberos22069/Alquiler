b2815a181815e76b281be641383fb63e1ae4e4e4
DiegoCV <skdiegomxtr@gmail.com>
2020-01-24 09:34:18 -0500

    Mostrar articulos esta finalizado

--- alquiler/front/factura_by_fecha.php
+++ alquiler/front/factura_by_fecha.php
@@ -361,9 +361,9 @@
                 mi_tr.appendChild( td(alquiler[i].dias, "footable-visible"));
                 //Total
                 total = alquiler[i].valor * alquiler[i].cantidad * alquiler[i].dias;
-                mi_tr.appendChild( td(total, "footable-visible"));
+                mi_tr.appendChild( td(number_format(total), "footable-visible"));
                 //Devoluciones
-                mi_tr.appendChild( td(alquiler[i].devoluciones, "footable-visible"));
+                mi_tr.appendChild(parsearDevoluciones(JSON.parse(alquiler[i].devoluciones)));
 
              contenedor.appendChild(mi_tr);
             }
@@ -381,13 +381,17 @@
         }
 
         function parsearDevoluciones(devoluciones){
-            rta = "";
+            var td = document.createElement("td");        
+            td.setAttribute("class", "footable-visible");
+            var ul = document.createElement("ul");        
             for(let i in devoluciones){
-                rta += "Fecha: "+devoluciones.fecha+", Cantidad: "+devoluciones.cantidad;
-                //No se le olvide poner el estado
+                var li = document.createElement("li");
+                rta = "Fecha: "+devoluciones[i].fecha+", Cantidad: "+devoluciones[i].cantidad+", Estado: "+obtenerEstado(devoluciones[i].estado);
+                li.appendChild(document.createTextNode(rta));
+                ul.appendChild(li)
             }
-
-
+            td.appendChild(ul);
+            return td;
         }
 
         function obtenerEstado(estado){
@@ -409,6 +413,28 @@
             }
         }
 
+        function number_format(amount, decimals) {
+
+            amount += ''; // por si pasan un numero en vez de un string
+            amount = parseFloat(amount.replace(/[^0-9\.]/g, '')); // elimino cualquier cosa que no sea numero o punto
+
+            decimals = decimals || 0; // por si la variable no fue fue pasada
+
+            // si no es un numero o es igual a cero retorno el mismo cero
+            if (isNaN(amount) || amount === 0) 
+                return parseFloat(0).toFixed(decimals);
+
+            // si es mayor o menor que cero retorno el valor formateado como numero
+            amount = '' + amount.toFixed(decimals);
+
+            var amount_parts = amount.split('.'),
+                regexp = /(\d+)(\d{3})/;
+
+            while (regexp.test(amount_parts[0]))
+                amount_parts[0] = amount_parts[0].replace(regexp, '$1' + ',' + '$2');
+
+            return amount_parts.join('.');
+        }
     </script>
 
     <script>
